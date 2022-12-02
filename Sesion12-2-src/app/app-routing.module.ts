0dc91df1b09419280768d1afa4f81807774b441a
MariaElvir <112521512+MariaElvir@users.noreply.github.com>
2022-12-02 17:20:35 -0500

    Add files via upload

---
+++ Sesion12-2-src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PadreComponent } from './padre/padre.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/main', pathMatch: 'full'},
+  {path: 'main', component: PadreComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
